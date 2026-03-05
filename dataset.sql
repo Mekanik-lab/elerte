a63f0f8f406ab4c739a2d9e2a4a671465d801db3
dawid <strzelecki002@wp.pl>
2026-03-05 14:46:52 +0100

    set danych do insert

---
+++ dataset.sql
@@ -0,0 +1,41 @@
+INSERT INTO magazyn (nazwa, kategoria, ilosc, lokalizacja, uwagi) VALUES
+('Patchcord RJ45 Cat.6A 1m', 'Okablowanie', 25, 'A-01-01', 'UTP, czarny'),
+('Patchcord RJ45 Cat.6A 2m', 'Okablowanie', 20, 'A-01-02', 'UTP, czarny'),
+('Patchcord RJ45 Cat.6A 3m', 'Okablowanie', 15, 'A-01-03', 'UTP, czarny'),
+('Patchcord RJ45 Cat.5e 1m', 'Okablowanie', 30, 'A-01-04', 'UTP, szary'),
+('Patchcord RJ45 Cat.5e 2m', 'Okablowanie', 22, 'A-01-05', 'UTP, szary'),
+
+('Wtyk RJ45 Cat.6A (opak. 50)', 'Złącza', 6, 'A-02-01', 'Ekranowany'),
+('Wtyk RJ45 Cat.5e (opak. 100)', 'Złącza', 4, 'A-02-02', 'Nieekranowany'),
+('Keystone RJ45 Cat.6A', 'Złącza', 18, 'A-02-03', 'Narzędziowy'),
+('Keystone RJ45 Cat.5e', 'Złącza', 25, 'A-02-04', 'Narzędziowy'),
+('Zaślepka RJ45 (opak. 10)', 'Złącza', 10, 'A-02-05', 'Gumowa'),
+
+('Skrętka Cat.6A U/UTP 305m', 'Okablowanie', 2, 'B-01-01', 'Pudełko'),
+('Skrętka Cat.5e UTP 305m', 'Okablowanie', 2, 'B-01-02', 'Pudełko'),
+('Organizer kabli 1U', 'Rack', 5, 'B-02-01', 'Plastik/metal'),
+('Listwa zasilająca rack 1U 6 gniazd', 'Rack', 3, 'B-02-02', 'Z wyłącznikiem'),
+('Patch panel 24p Cat.6A', 'Rack', 4, 'B-02-03', '19"'),
+
+('Switch 8p Gigabit (niezarządzalny)', 'Sieć', 4, 'C-01-01', 'Biuro/magazyn'),
+('Switch 24p Gigabit (zarządzalny)', 'Sieć', 1, 'C-01-02', 'Szafa rack'),
+('Access Point Wi-Fi', 'Sieć', 6, 'C-01-03', 'Różne modele'),
+('Injectory PoE', 'Sieć', 5, 'C-01-04', '48V'),
+('Moduł SFP 1G MM', 'Sieć', 6, 'C-02-01', 'LC, multimode'),
+
+('Transceiver SFP 1G SM', 'Sieć', 2, 'C-02-02', 'LC, singlemode'),
+('Patchcord światłowodowy LC-LC OM3 2m', 'Światłowód', 8, 'C-02-03', 'Aqua'),
+('Pigtail LC OM3', 'Światłowód', 12, 'C-02-04', 'Do przełącznicy'),
+('Adapter LC duplex', 'Światłowód', 10, 'C-02-05', 'Do patch panelu'),
+('Kaseta/organizer spawów', 'Światłowód', 4, 'C-03-01', 'Do przełącznicy'),
+
+('Zasilacz 12V 2A', 'Zasilanie', 6, 'D-01-01', 'Do kamer/IT'),
+('Bateria CR2032', 'Zasilanie', 20, 'D-01-02', 'Do płyt/urządzeń'),
+('Bateria AA (opak. 10)', 'Zasilanie', 8, 'D-01-03', 'Alkaliczne'),
+('Przedłużacz 5m', 'Zasilanie', 6, 'D-01-04', 'Listwa 3 gniazda'),
+('UPS 650VA', 'Zasilanie', 1, 'D-01-05', 'Testowy, biuro'),
+
+('Dysk SSD 500GB', 'Serwis', 3, 'E-01-01', 'SATA'),
+('Pendrive 32GB', 'Serwis', 10, 'E-01-02', 'Do instalacji'),
+('Zestaw śrubek do rack (M6)', 'Rack', 10, 'E-01-03', 'Komplet: śruba+koszyk+podkładka'),
+('Opaski zaciskowe (trytytki) 100 szt.', 'Akcesoria', 12, 'E-01-04', 'Czarne');
